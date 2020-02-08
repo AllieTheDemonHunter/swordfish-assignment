4429cce93f0af41a197dd680520bb77a05c7b802
Allie du Plooy <allie.rockdev@gmail.com>
2020-02-08 15:08:05 +0200

    Debugging.

--- index.php
+++ index.php
@@ -6,16 +6,6 @@ use gitHubController;
 session_start();
 include_once 'gitHubController.php';
 include_once 'gitHubView.php';
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Swordfish Application Test</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-<?php
-
 
 $gitHub = new gitHubController();
 $base = new Base($gitHub->response);
@@ -29,6 +19,18 @@ $newIssue = [
     'assignees' => ['AllieTheDemonHunter']
 ];
 $labels = $gitHub->apiRequest($labelsUrl, $newIssue);
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Swordfish Application Test</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php
+
+
+
 print_r($labels);
 ?>
 </body>
