f2bdc53d704e6bf32069f63dad58607c8641989f
Allie du Plooy <allie.rockdev@gmail.com>
2020-02-10 09:55:17 +0200

    Debugging.

--- gitHubController.php
+++ gitHubController.php
@@ -35,8 +35,8 @@ class gitHubController
         //Making life easier.
         $this->base_url = PROTOCOL . '://' . DOMAIN . '/' . APP_NAME_LOCAL;
         $this->access_token = $this->session('access_token')->access_token;
+        print_r($_REQUEST);
         if ($this->access_token) {
-print_r($_REQUEST);
             return $this;
         }
         if ($this->get('code')) {
