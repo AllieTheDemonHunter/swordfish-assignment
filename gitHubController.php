3b01140fe4ede1c4f59b52e8927956cca42bd83a
Allie du Plooy <allie.rockdev@gmail.com>
2020-02-12 00:00:37 +0200

    Step 46

--- gitHubController.php
+++ gitHubController.php
@@ -1,13 +1,13 @@
 <?php
-define('OAUTH2_CLIENT_ID', '53af175ce5d46b80f33a');
-define('OAUTH2_CLIENT_SECRET', '9f906210cbce17ccc66cb97050e3c7d22bdfa4ac');
+define('OAUTH2_CLIENT_ID', '2434d612549dff0bb4e0');
+define('OAUTH2_CLIENT_SECRET', 'b815281ba8cd9cc295b4b6bc1ed375da8d50ad61');
 
 define('APP_NAME', 'GitIntegration');
 define('OAUTH_APP_NAME', 'swordhunter');
 define('GITHUB_ACCOUNT', 'AllieTheDemonHunter');
 define('REPO_NAME', 'GitIntegration');
-define('DOMAIN', 'allie.co.za');
-define('PROTOCOL', 'https'); //Enforcing this, sorry, not sorry.
+define('DOMAIN', 'localhost:8080');
+define('PROTOCOL', 'http'); //Enforcing this, sorry, not sorry.
 define('AUTH_URL', 'https://github.com/login/oauth/authorize');
 define('TOKEN_URL', 'https://github.com/login/oauth/access_token');
 define('API_URL', 'https://api.github.com');
