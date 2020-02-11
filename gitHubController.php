bffa8cac5591999f091d97c9793e9d5ef3397601
Allie du Plooy <allie.rockdev@gmail.com>
2020-02-11 23:48:33 +0200

    Step 46

--- gitHubController.php
+++ gitHubController.php
@@ -129,7 +129,7 @@ class gitHubController
         }
 
         $headers[] = 'User-Agent:' . OAUTH_APP_NAME;
-        $headers[] = 'Accept: application/vnd.github.v3+json';
+        $headers[] = 'Accept: application/json';
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         $this->response = curl_exec($ch);
         /**
