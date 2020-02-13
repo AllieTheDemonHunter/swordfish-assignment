57da33f41ea24239150ff2fdc26096688a4b57f0
Allie du Plooy <allie.rockdev@gmail.com>
2020-02-13 17:46:36 +0200

    Step 100

--- gitHubController.php
+++ gitHubController.php
@@ -117,6 +117,7 @@ class gitHubController
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLINFO_HEADER_OUT, 1);
         curl_setopt($ch, CURLOPT_VERBOSE, 1);
+
         if ($post) {
             curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post));
         }
@@ -129,9 +130,25 @@ class gitHubController
         $headers[] = 'User-Agent: ' . OAUTH_APP_NAME;
         $headers[] = 'Accept: application/vnd.github.v3+json';
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        $this->response = curl_exec($ch);
 
+        curl_setopt($ch, CURLOPT_HEADERFUNCTION,
+            function($curl, $header) use (&$headers)
+            {
+                $len = strlen($header);
+                $header = explode(':', $header, 2);
+                if (count($header) < 2) // ignore invalid headers
+                    return $len;
+
+                $headers[strtolower(trim($header[0]))][] = trim($header[1]);
+
+                return $len;
+            }
+        );
+
+
+        $this->response = curl_exec($ch);
         $this->debug[] = curl_getinfo($ch);
+        $this->debug[] = $headers;
 
         return json_decode($this->response);
     }
