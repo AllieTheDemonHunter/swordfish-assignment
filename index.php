e7d8a67a926f1dfe13b2e29dde4a4c9e6cec7a91
Allie du Plooy <allie.rockdev@gmail.com>
2020-02-14 14:14:07 +0200

    Step 100

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ session_start();
 include_once 'gitHubController.php';
 include_once 'gitHubView.php';
 $gitHub = new gitHubController();
-print_r($_SESSION);
-print_r($gitHub);
 $base = new Base($gitHub->response);
 print $base;
+print_r($gitHub);
